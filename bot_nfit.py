59d8cbded20ab9aff34c49ae508f8ca77d0981b4
franciscolodi <62471145+franciscolodi@users.noreply.github.com>
2025-10-06 17:11:55 -0300

    bot_nfit.py

--- bot_nfit.py
+++ bot_nfit.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import selenium.webdriver.support.expected_conditions as EC
 from telegram import Bot
 
 # --- Cargar variables desde .env ---
