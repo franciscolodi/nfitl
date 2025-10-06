af12f88a5ef7b725b0bca7b004c339930cbd73bd
franciscolodi <62471145+franciscolodi@users.noreply.github.com>
2025-10-06 17:34:30 -0300

    bot_nfit.py

--- bot_nfit.py
+++ bot_nfit.py
@@ -29,12 +29,15 @@ def send_telegram(msg):
     print(msg)  # También imprimir en consola
 
 # --- Configuración del navegador (modo headless) ---
+
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+options.add_argument('--headless=new')  # 👈 Usa el nuevo modo headless
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument("start-maximized")
-options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--start-maximized')
+options.add_argument('--disable-blink-features=AutomationControlled')
 
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 12)
@@ -59,7 +62,7 @@ try:
         (By.XPATH, '/html/body/div/div/div/main/section/form/div[4]/button')))
     pedido_option.click()
     send_telegram("Inicio de sesión realizado correctamente.")
-    time.sleep(2)
+    time.sleep(4)
 
     menu_clases = wait.until(EC.element_to_be_clickable(
         (By.XPATH, '//*[@id="menu"]/nav/ul/a[3]')))
